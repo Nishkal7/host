f6a7a0aac9b57cfc4c259008aaca2092c0713458
Nishkal7 <nishkal8463@gmail.com>
2024-08-11 15:11:52 +0530

    build test

--- onboarding/src/ErrorBoundary.js
+++ onboarding/src/ErrorBoundary.js
@@ -17,7 +17,7 @@ class ErrorBoundary extends Component {
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong</h1>;
+      return <h1>Something went wrong, Please try again later</h1>;
     }
     return this.props.children;
   }
